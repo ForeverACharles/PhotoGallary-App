f5b808ee792ad5ee9ad7f7502e7fe1e3cb6f2ce7
Max Sun <ms2814@scarletmail.rutgers.edu>
2021-04-12 17:06:26 -0400

    Updated album for earliest and latest dates, getters

--- Photos55/src/Photos55/app/Album.java
+++ Photos55/src/Photos55/app/Album.java
@@ -6,5 +6,42 @@ public class Album implements Serializable {
 
 	private String name;
 	private ArrayList<Photo> photos;
-	
+	public Album(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public int getLength() {
+		return this.photos.size();
+	}
+	public ArrayList<Photo> getPhotos(){
+		return this.photos;
+	}
+	public String getEarliest() {
+		if (this.getLength()== 0) {
+			return null;
+		}
+		Calendar current = photos.get(0).getDate();
+		for (Photo photo : photos) {
+			if (photo.getDate().before(current)) {
+				current = photo.getDate();
+			}
+		}
+		return current.toString();
+		
+	}
+	public String getLatest() {
+		if (this.getLength()== 0) {
+			return null;
+		}
+		Calendar current = photos.get(0).getDate();
+		for (Photo photo : photos) {
+			if (photo.getDate().after(current)) {
+				current = photo.getDate();
+			}
+		}
+		return current.toString();
+		
+	}
 }
